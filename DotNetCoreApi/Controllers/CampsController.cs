099ce4398695dcc3a592f96fa4d53cb9c7c4b51f
martins <martin.short@qnrl.com>
2019-09-12 13:54:33 +0100

    Add initial code camp project

---
+++ DotNetCoreApi/Controllers/CampsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });
+        }
+    }
+}
