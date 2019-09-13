3f00d7071682d306e8bbca19198ba141e17c6f25
martins <martin.short@qnrl.com>
2019-09-13 14:47:33 +0100

    Add explicit mapping for Venue property

--- DotNetCoreApi/Data/CampProfile.cs
+++ DotNetCoreApi/Data/CampProfile.cs
@@ -7,7 +7,11 @@ namespace CoreCodeCamp.Data
     {
         public CampProfile()
         {
-            CreateMap<Camp, CampModel>();
+            CreateMap<Camp, CampModel>()
+                // Just showing how you can have more control over the mapping
+                // of object properties e.g. in this case we want to have the property
+                // name of "Venue" in CampModel, but still have it auto mapped
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
         }
     }
 }
