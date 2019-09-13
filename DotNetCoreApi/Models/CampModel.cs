61233f7bdf64284ee9054dfc5fc3f98a38af8814
martins <martin.short@qnrl.com>
2019-09-13 14:01:47 +0100

    Update CampModel to return related location data

--- DotNetCoreApi/Models/CampModel.cs
+++ DotNetCoreApi/Models/CampModel.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CoreCodeCamp.Models
 {
@@ -12,5 +8,14 @@ namespace CoreCodeCamp.Models
         public string Moniker { get; set; }
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
+
+        public string LocationVenueName { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
     }
 }
