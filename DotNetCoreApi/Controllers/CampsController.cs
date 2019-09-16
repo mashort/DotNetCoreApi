f7e1707aa0d7978b6e0f4b82eea14304a4bb288f
martins <martin.short@qnrl.com>
2019-09-16 16:54:50 +0100

    Add implementation for POST camp

--- DotNetCoreApi/Controllers/CampsController.cs
+++ DotNetCoreApi/Controllers/CampsController.cs
@@ -3,6 +3,7 @@ using CoreCodeCamp.Data;
 using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,15 +11,18 @@ using System.Threading.Tasks;
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
+    [ApiController]
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository _repository;
         private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
 
-        public CampsController(ICampRepository repository, IMapper mapper)
+        public CampsController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
         {
             _repository = repository;
             _mapper = mapper;
+            _linkGenerator = linkGenerator;
         }
 
         [HttpGet]
@@ -70,6 +74,43 @@ namespace CoreCodeCamp.Controllers
 
                 return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
             }
+        }
+
+        public async Task<ActionResult<CampModel>> Post(CampModel model)
+        {
+            try
+            {
+                // LinkGenerator is available in ASP.NET Core 2.2+
+                // and can be used to dynamically get the Uri of the newly created resource
+                var location = _linkGenerator.GetPathByAction("Get",
+                    "Camps",
+                    new { moniker = model.Moniker });
+
+                if (string.IsNullOrWhiteSpace(location))
+                {
+                    return BadRequest("Could not use current moniker");
+                }
+                
+                // Take the CampModel and use AutoMapper to map it back to a Camp entity before adding it to the data store
+                var camp = _mapper.Map<Camp>(model);
+                _repository.Add(camp);
+
+                if (await _repository.SaveChangesAsync())
+                {
+                    // After the resource has been created map the Camp entity to a Camp Model
+                    // for returning in the body along with to the Uri of the newly created Camp
+                    return Created(location, _mapper.Map<CampModel>(camp));
+                }
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+
+            // If something bad happens that doesn't cause an exception
+            return BadRequest();
         }
     }
 }
--- DotNetCoreApi/Data/CampProfile.cs
+++ DotNetCoreApi/Data/CampProfile.cs
@@ -11,7 +11,7 @@ namespace CoreCodeCamp.Data
                 // Just showing how you can have more control over the mapping
                 // of object properties e.g. in this case we want to have the property
                 // name of "Venue" in CampModel, but still have it auto mapped
-                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName)).ReverseMap();
         }
     }
 }
