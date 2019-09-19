21672c8ebabc807f555627f632e6ee8c90658928
martins <martin.short@qnrl.com>
2019-09-19 12:27:42 +0100

    Add (Functional API) endpoint for reload config

---
+++ DotNetCoreApi/Controllers/OperationsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        // This is an example of a Functional API endpoint (not resource based).
+        // Tip: If you find yourself writing a lot of these in your REST API solution,
+        // then it's a sign that these should be in a RPC (Remote Procedure Call) over HTTP solution.
+        // Be pragmatic when building REST APIs!
+
+        private readonly IConfiguration _config;
+
+        public OperationsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)_config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
