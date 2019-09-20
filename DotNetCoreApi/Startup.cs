24ee1b2b4f5a5a47b78976294b01aa07701016d9
martins <martin.short@qnrl.com>
2019-09-20 14:26:07 +0100

    Experiment with Api versioning schemes

--- DotNetCoreApi/Startup.cs
+++ DotNetCoreApi/Startup.cs
@@ -3,6 +3,7 @@ using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CoreCodeCamp
@@ -21,6 +22,8 @@ namespace CoreCodeCamp
                 opt.AssumeDefaultVersionWhenUnspecified = true;
                 opt.DefaultApiVersion = new ApiVersion(1, 1);
                 opt.ReportApiVersions = true;
+                //opt.ApiVersionReader = new QueryStringApiVersionReader();
+                //opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");
             });
 
             services.AddMvc()
